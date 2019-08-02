dca40c1c0d5c868ff140faed325e98562d03daf6
Glitch (support-assets) <none>
2019-08-02 16:41:18 +0000

    🍭🚆 Updated with Glitch

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-This is a web encyclopedia of graphical programs and demonstrations.  They all include a small file tinywebgl-ucla.js, a tutorial file from UCLA's Graphics Programming class that minimally sets up a  WebGL 3D drawing program for any purpose.  
+Support Assets
+=================
 
-Each demo/article in this encyclopedia shows a scene, made by a single subclass of Scene_Component from tiny-graphics.js.   Users can view the source of each Scene_Component subclass, and easily code and contribute their own in the same textbox, submitting a new article to the database for all to see.
+Screenshots and screencast gifs are often useful to describe activities that would take a lot more effort to describe in words - not to be too trite, but sometimes a picture (or animation) really *is* worth a thousand words.
 
-You can write and submit more classes to the database (besides just scenes), and then depend on those later in your actual Scene_Component.  Two more classes from tinywebgl-ucla.js you might want to subclass are Shape and Shader.
+Files in this project's assets "drawer" are stored here for external reference in places like support.glitch.com and glitch.com/help.
+
+Can also be used to save images of what Support staff are seeing while trying to repro a problem.
+
+### Best Practices
+If you add an image to the assets it should be accompanied by a "route" to it in lws.config.js (they're called `rewrite`s there). That will allow us to use simple urls for linking and to keep them updated (but at the same url) when things change.
+
+For some reason that hasn'tr been sorted out yet, sometimes those `rewrite`s don't always play well with S3-stored gifs, so we typically download them to the local `/img` directory and point to them there.
+
+`wget {asset url} -O /img/{filename}` suffices for this.
+
+Likewise if you provide an updated asset it makes sense to update any existing redirects.
+
+Also please name things descriptively so other folks can tell what the heck they're for.
+
+\ ゜o゜)ノ
---
+++ lws.config.js
@@ -0,0 +1,110 @@
+module.exports = {
+  directory: '/app/img',
+  logFormat: 'stats',
+  rewrite: [
+    {
+      from: '/assets-drawer',
+      to: 'assets-drawer.gif'
+    },
+    {
+      from: '/assets-drawer-old',
+      to: 'https://cdn.glitch.com/32df0d2f-f0eb-4c35-86aa-228e2befb282%2Ftoolbox-assets-drawer.gif?1549345653104'
+    },
+    {
+      from: '/logs-pane-console',
+      to: 'toolbox-logs-console.gif'
+    },
+    {
+      from: '/red-dot-error',
+      to: 'red-dot-error.gif'
+    },
+    {
+      from: '/new-stuff-toolbox',
+      to: 'new-stuff-toolbox.gif'
+    },
+    {
+      from: '/toolbox-logs',
+      to: 'new-tools-logs.gif'
+    },
+    {
+      from: '/invite-link',
+      to: 'invite-link.gif'
+    },
+    {
+      from: '/download-project',
+      to: 'toolbox-download-project.gif'
+    },
+    {
+      from: '/forums-message-staff',
+      to: 'discourse-add-staff-to-message.gif'
+    },
+    {
+      from: '/git-username',
+      to: 'toolbox-git-username.gif'
+    },
+    {
+      from: '/enable-pnpm',
+      to: 'toolbox-enable-pnpm.gif'
+    },
+    {
+      from: '/git-username-url',
+      to: 'toolbox-git-username-url.gif'
+    },
+    {
+      from: '/refresh-app',
+      to: 'refresh-app-2019-07-22.gif'
+    },
+    {
+      from: '/remove-from-project',
+      to: 'remove-from-project.gif'
+    },
+    {
+      from: '/download-git-import-export',
+      to: 'tools-dl-git-export.gif'
+    },
+    {
+      from: '/console-options',
+      to: 'console-options.gif'
+    },
+    {
+      from: '/project-privacy',
+      to: 'project-privacy.gif'
+    },
+    {
+      from: '/editor-project-name-description',
+      to: 'editor-project-name-description.gif'
+    },
+    {
+      from: '/editor-project-avatar',
+      to: 'editor-project-avatar.gif'
+    },
+    {
+      from: '/editor-keyboard-shortcuts',
+      to: 'editor-keyboard-shortcuts.gif'
+    },
+    {
+      from: '/custom-domains',
+      to: 'custom-domains.gif'
+    },
+    {
+      from: '/debugger',
+      to: 'debugger.gif'
+    },
+    {
+      from: '/change-theme',
+      to: 'change-theme.gif'
+    },
+    {
+      from: '/add-license',
+      to: 'add-license.gif'
+    },
+    {
+      from: '/github-grant-access',
+      to: 'github-grant-access.gif'
+    },
+    {
+      from: '/pre-release.vsix',
+      to: 'glitch-0.1.2-pre2.vsix'
+    }
+  ]
+}
